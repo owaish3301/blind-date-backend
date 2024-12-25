af4a22a7fba7b6c3aa12869251129c5fd10c1373
owaish3301 <owaish3301@gmail.com>
2024-12-25 11:34:37 +0530

    vercel fix

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ app.set('io', io);
 app.set('emitCardUpdate', emitCardUpdate);
 
 // Make sure server.listen is called
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Make sure to export both app and server
 module.exports = server;
